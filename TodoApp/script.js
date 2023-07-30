0b9d1957038bc359a1db7d9040390ad5948de819
MirzeHusheynov <mirzeh32@gmail.com>
2023-07-30 17:05:50 +0400

    update

---
+++ TodoApp/script.js
@@ -0,0 +1,22 @@
+let textInput = document.querySelector("#textInput"),
+addText = document.querySelector("#addText"),
+clearAll = document.querySelector("#clearAll"),
+addParagraph = document.querySelector("#addParagraph");
+
+addText.addEventListener('click',function(){
+    let paragraf = document.createElement("p")
+    addParagraph.appendChild(paragraf);
+    paragraf.innerHTML = textInput.value;
+    textInput.value = ""
+    paragraf.classList.add("paragraf")
+    paragraf.addEventListener('click',function(){
+        paragraf.style.textDecoration = "line-through"
+        paragraf.style.color = "blue"
+    })
+    clearAll.addEventListener('click',function(){
+        paragraf.remove()
+    })
+    paragraf.addEventListener('dblclick',function(){
+        addParagraph.removeChild(paragraf)
+    })
+})
